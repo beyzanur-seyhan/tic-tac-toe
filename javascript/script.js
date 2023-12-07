08690f758bac91c0b44dd6810844bfc3a3df16b7
Beyzanur Seyhan <seyhanbeyzanur836@gmail.com>
2023-12-07 22:52:44 +0300

    create computer order

--- javascript/script.js
+++ javascript/script.js
@@ -6,17 +6,22 @@ let userOrder = 'X';
 document.getElementById('score-user').textContent = scoreUser;
 document.getElementById('score-computer').textContent = scoreComputer;
 
-const setUserFlag = (order) => {
+const createFlag = (order,flag='X') => {
+    if(cellValues[order] && flag === 'O') turnComputer(1);
     if(cellValues[order]) return;
 
     const divContainer = document.getElementsByTagName('main')[0];
     const selectedCell = divContainer.querySelectorAll('div')[order];
-    cellValues[order] = userOrder;
+    cellValues[order] = flag;
 
     const imageTag = document.createElement('img');
 
-    imageTag.src = `image/shape-${userOrder}.svg`;
+    imageTag.src = `image/shape-${flag}.svg`;
     selectedCell.appendChild(imageTag);
+}
 
-    userOrder = userOrder === 'X' ? 'O' : 'X';
+const turnComputer = (time=1000) => {
+    setTimeout(()=>{
+        createFlag(Math.round(Math.random() * 8), 'O')
+    },time)
 }
